ab46ab6844b09cfec577d44ab4bb610c114c0aef
Alex Sullivan <115666116+asullivan-blze@users.noreply.github.com>
2023-10-04 14:35:47 -0700

    boardwalk (bug): add model field annotations
    Missing annotations lead to runtime errors from Pydantic; also bump Pydantic to v2.

--- src/boardwalk/host.py
+++ src/boardwalk/host.py
@@ -28,12 +28,12 @@ class Host(BaseModel, extra=Extra.forbid):
     ansible_facts: dict[str, Any]
     name: str
     meta: dict[str, str | int | bool] = {}
-    remote_mutex_path = "/opt/boardwalk.mutex"
-    remote_alert_msg = "ALERT: Boardwalk is running a workflow against this host. Services may be interrupted"
-    remote_alert_string_formatted = f"$(tput -T xterm bold)$(tput -T xterm setaf 1)'{remote_alert_msg}'$(tput -T xterm sgr0)"
-    remote_alert_motd = f"#!/bin/sh\necho {remote_alert_string_formatted}"
-    remote_alert_motd_path = "/etc/update-motd.d/99-boardwalk-alert"
-    remote_alert_wall_cmd = f"wall {remote_alert_string_formatted}"
+    remote_mutex_path: str = "/opt/boardwalk.mutex"
+    remote_alert_msg: str = "ALERT: Boardwalk is running a workflow against this host. Services may be interrupted"
+    remote_alert_string_formatted: str = f"$(tput -T xterm bold)$(tput -T xterm setaf 1)'{remote_alert_msg}'$(tput -T xterm sgr0)"
+    remote_alert_motd: str = f"#!/bin/sh\necho {remote_alert_string_formatted}"
+    remote_alert_motd_path: str = "/etc/update-motd.d/99-boardwalk-alert"
+    remote_alert_wall_cmd: str = f"wall {remote_alert_string_formatted}"
 
     def ansible_run(
         self,
