736c60e3b060aa3b2cb13243e722d7b2f723ea03
Alex Sullivan <115666116+asullivan-blze@users.noreply.github.com>
2024-11-26 10:27:05 -0800

    boardwalk (bug): Ensure workflow is set as succeeded false when beginning a workflow (#105)
    * boardwalk (bug): Ensure workflow is set as succeeded false when beginning a workflow
    
    This PR adds a test/fix to ensure that when reusing a workspace -- for example, updating
    the target version to upgrade an operating system to -- the remote state file is updated
    to reflect the fact that the Workspace's Workflow has not yet successfully completed,
    prior to said workflow actually beginning.
    
    This ensures that if the reused Workspace's workflow completed successfully, a failure
    within the current workflow won't requite manual user intervention to reset the remote
    state file's workflow success attribute to false. This allows the workflow to be resumed,
    as prior to this if a host met preconditions, and as a result of the workflow they
    altered to a state where they are no longer met -- e.g., an OS upgrade -- the workflow
    would refuse to allow resumption of the failed workflow, because -- to Boardwalk -- the
    host already succeeded.
    
    Compatible with previous release.

--- test/integration/test_workspaces.py
+++ test/integration/test_workspaces.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import re
 import signal
 from pathlib import Path
 from typing import Any
@@ -7,6 +8,52 @@ from typing import Any
 import pytest
 from anyio import create_task_group, fail_after, open_process
 from anyio.streams.text import TextReceiveStream
+
+
+async def execute_boardwalk_workspace_test(
+    workspace_name: str,
+    failure_expected: bool,
+    failure_msg: str,
+    get_become_password_file_path: Path,
+    use_isolated_boardwalk_directory,
+):
+    WORKSPACE_CAUGHT_REGEX = re.compile(
+        rf"The {workspace_name} workspace is remotely caught on .+ Waiting for release before continuing"
+    )
+    envvars: dict[str, Any] = {
+        "ANSIBLE_BECOME_PASSWORD_FILE": get_become_password_file_path,
+        "BOARDWALK_VERBOSITY": "2",
+    }
+    new_environ = os.environ | envvars
+    new_environ.pop("ANSIBLE_BECOME_ASK_PASS", None)
+    commands: tuple[str, ...] = (
+        f"boardwalk -vv workspace use {workspace_name}",
+        "boardwalk init",
+        "boardwalk run",
+    )
+    output_stdout = []
+    output_stderr = []
+    os.chdir(use_isolated_boardwalk_directory)
+    async with create_task_group():
+        for command in commands:
+            with fail_after(delay=90) as scope:
+                async with await open_process(command=command, env=new_environ) as process:
+                    async for text in TextReceiveStream(process.stdout):  # type:ignore
+                        # To allow for reading what was received, if the test ends up failing.
+                        print(text)
+                        output_stdout.append(text)
+                        if re.search(WORKSPACE_CAUGHT_REGEX, text):
+                            process.send_signal(signal.SIGINT)
+                    async for text in TextReceiveStream(process.stderr):  # type:ignore
+                        print(text)
+                        output_stderr.append(text)
+        assert not scope.cancelled_caught
+
+    if failure_expected:
+        assert failure_msg in "".join(output_stdout)
+    else:
+        assert "Host completed successfully; wrapping up" in "".join(output_stdout)
+        assert process.returncode == 0
 
 
 @pytest.mark.anyio
@@ -58,38 +105,37 @@ async def test_development_workspaces(
     get_become_password_file_path: Path,
     use_isolated_boardwalk_directory,
 ):
-    envvars: dict[str, Any] = {
-        "ANSIBLE_BECOME_PASSWORD_FILE": get_become_password_file_path,
-        "BOARDWALK_VERBOSITY": "2",
-    }
-    new_environ = os.environ | envvars
-    new_environ.pop("ANSIBLE_BECOME_ASK_PASS", None)
-    commands: tuple[str, ...] = (
-        f"boardwalk -vv workspace use {workspace_name}",
-        "boardwalk init",
-        "boardwalk run",
+    await execute_boardwalk_workspace_test(
+        workspace_name=workspace_name,
+        failure_msg=failure_msg,
+        failure_expected=failure_expected,
+        get_become_password_file_path=get_become_password_file_path,
+        use_isolated_boardwalk_directory=use_isolated_boardwalk_directory,
     )
-    output_stdout = []
-    output_stderr = []
-    os.chdir(use_isolated_boardwalk_directory)
-    async with create_task_group():
-        for command in commands:
-            with fail_after(delay=90) as scope:
-                async with await open_process(command=command, env=new_environ) as process:
-                    async for text in TextReceiveStream(process.stdout):  # type:ignore
-                        # To allow for reading what was received, if the test ends up failing.
-                        print(text)
-                        output_stdout.append(text)
-                        if failure_expected and "Waiting for release before continuing" in text:
-                            process.send_signal(signal.SIGINT)
-                    async for text in TextReceiveStream(process.stderr):  # type:ignore
-                        print(text)
-                        output_stderr.append(text)
 
-        assert not scope.cancelled_caught
 
-    if failure_expected:
-        assert failure_msg in "".join(output_stdout)
-    else:
-        assert "Host completed successfully; wrapping up" in "".join(output_stdout)
-        assert process.returncode == 0
+@pytest.mark.anyio
+@pytest.mark.skipif(condition="CI" in os.environ, reason="Not yet able to execute non-interactively.")
+async def test_ensure_remote_workflow_success_state_false_during_workflow_execution(
+    get_become_password_file_path: Path,
+    use_isolated_boardwalk_directory,
+    workspace_name: str = "RemoteStateShouldHaveWorkflowSucceededValueSetAsFalseDuringExecutionWorkspace",
+    failure_expected: bool = False,
+    failure_msg: str = "",
+):
+    # This first execution ensures the Workspace completes at least once (ensuring the state is set)
+    await execute_boardwalk_workspace_test(
+        workspace_name=workspace_name,
+        failure_msg=failure_msg,
+        failure_expected=failure_expected,
+        get_become_password_file_path=get_become_password_file_path,
+        use_isolated_boardwalk_directory=use_isolated_boardwalk_directory,
+    )
+    # This second execution actually verifies that the succeeded state is False
+    await execute_boardwalk_workspace_test(
+        workspace_name=workspace_name,
+        failure_msg=failure_msg,
+        failure_expected=failure_expected,
+        get_become_password_file_path=get_become_password_file_path,
+        use_isolated_boardwalk_directory=use_isolated_boardwalk_directory,
+    )
